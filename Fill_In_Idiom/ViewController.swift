a0299decebd2c97a773fe8b68591ad1fa12c5a24
Jack Chen <taiyun0614@gmail.com>
2020-06-04 18:50:38 +0800

    Initial Commit

---
+++ Fill_In_Idiom/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Fill_In_Idiom
+//
+//  Created by Tai-Yun Chen on 2020/6/4.
+//  Copyright © 2020 Jack Chen. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
